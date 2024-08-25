31b23e1f2bb3ff217ada08b9b22808f46f7f5c1b
wlora <wlorat@gmail.com>
2024-08-25 12:29:26 -0500

    add config-data user-service

--- src/main/java/org/virtualizat/config/ConfigApp.java
+++ src/main/java/org/virtualizat/config/ConfigApp.java
@@ -2,12 +2,14 @@ package org.virtualizat.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class ConfigServiceApplication {
+public class ConfigApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
+		SpringApplication.run(ConfigApp.class, args);
 	}
 
 }
